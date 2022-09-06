0aaeba3658845678be98111068e52a72084a2850
miguel04101719 <madiaz@escuelasproa.edu.ar>
2022-09-06 10:08:14 -0300

    hoy musha paja

---
+++ actividad1.0.js
@@ -0,0 +1,39 @@
+
+
+function crearLabe(){
+    let mz = [["s",".",".",".","#","#"],
+              ["#",".","#",".",".","."],
+              ["#",".","#","#",".","#"],
+              [".",".","#",".","#","#"],
+              ["#",".",".",".","#","G"],
+              ["#",".","#",".",".","."]]
+    console.log(mz)
+}
+
+crearLabe()
+
+
+
+function resol(mz,) {
+    
+}
+
+/*
+if (x,y outside maze) 
+return false
+if (x,y is goal) 
+return true
+if (x,y not open)
+ return false
+mark x,y as part of solution path
+if (FIND-PATH(North of x,y) == true) 
+return true
+if (FIND-PATH(East of x,y) == true) 
+return true
+if (FIND-PATH(South of x,y) == true) 
+return true
+if (FIND-PATH(West of x,y) == true) 
+return true
+unmark x,y as part of solution path
+return false
+*/
