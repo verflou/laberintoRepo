f102ff0c9bee714088e0e564ddc77955d9c54c25
Tu Nombre <you@example.com>
2022-09-13 10:01:57 -0300

    Joaco es puto y Fran no

--- actividad1.0.js
+++ actividad1.0.js
@@ -1,21 +1,45 @@
 
+let maze = crearLabe();
 
 function crearLabe(){
-    let mz = [["s",".",".",".","#","#"],
-              ["#",".","#",".",".","."],
-              ["#",".","#","#",".","#"],
-              [".",".","#",".","#","#"],
-              ["#",".",".",".","#","G"],
-              ["#",".","#",".",".","."]]
-    console.log(mz)
+    let mz = [["s",".","."],
+              ["#",".","#"],
+              ["#",".","G"]];
+    return mz
 }
 
-crearLabe()
+for (let i = 0; i < maze.length; i++) {
+    for (let j = 0; j < maze.length; j++) {
+        if (maze[i][j] == "s") {
+            resol(maze, j, i);
+        }
+    }
+}
 
+function resol(mz, x, y) {
+    if (x == -1 || y == -1 || x == mz.length+1 || y == mz.length+1) {
+        return false
+    }
+    if (mz[x][y] == "G") {
+        return true
+    }
+    if (mz[x][y] == "#") {
+        return false
+    }
+    mz[x][y] == "x";
+    if (resol(mz, x, y+1) == true) {
+        return true
+    }
+    if (resol(mz, x+1, y) == true) {
+        return true
+    }
+    if (resol(mz, x, y-1) == true) {
+        return true
+    }
+    if (resol(mz, x-1, y) == true) {
+        return true
+    }
 
-f
-function resol(mz,) {
-    
 }
 
 /*
@@ -34,6 +58,8 @@ if (FIND-PATH(South of x,y) == true)
 return true
 if (FIND-PATH(West of x,y) == true) 
 return true
+
+
 unmark x,y as part of solution path
 return false
 */
