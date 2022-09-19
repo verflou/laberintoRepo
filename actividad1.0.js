b2986aa9e6abce1e21275761baf4ed722e1a73ee
Franco Machado <fmachado@escuelasproa.edu.ar>
2022-09-19 06:39:28 -0300

    inombredilpadridelfiliodelspiritusantiamen

--- actividad1.0.js
+++ actividad1.0.js
@@ -1,65 +1,100 @@
+let a = 0;
+let b = 0;
+let maze = MkRandMz();
+maze[b][a] = "S";
 
-let maze = crearLabe();
+if (resol(maze, a, b) == true) {
+    console.log("!Encontro el camino!");
+    mostrar();
+} else {
+    console.log("No encontro el camino...");
+}
 
-function crearLabe(){
-    let mz = [["s",".","."],
-              ["#",".","#"],
-              ["#",".","G"]];
+// CREAR LABERINTO
+/*
+function crearLabe() {
+    let mz = [
+        [".", ".", ".", ".", "."],
+        ["#", "#", "#", "#", "."],
+        [".", ".", ".", ".", "."],
+        [".", "#", ".", "#", "."],
+        [".", "#", ".", "#", "G"],
+    ];
     return mz
 }
+*/
 
-for (let i = 0; i < maze.length; i++) {
-    for (let j = 0; j < maze.length; j++) {
-        if (maze[i][j] == "s") {
-            resol(maze, j, i);
+//INTENTO CREAR LABERINTO ALEATORIO
+function MkRandMz() {
+    let mz = [
+        [".", ".", ".", ".", "."],
+        [".", ".", ".", ".", "."],
+        [".", ".", ".", ".", "."],
+        [".", ".", ".", ".", "."],
+        [".", ".", ".", ".", "."],
+    ];
+    mz[Math.floor(Math.random() * (5 - 3 + 1) ) + 3][
+        Math.floor(Math.random() * (5 - 3 + 1) ) + 3
+    ] = "G";
+    for (let i = 0; i < mz.length; i++) {
+        for (let j = 0; j < mz.length; j++) {
+            let nro = Math.floor(Math.random() * 4);
+            if (nro !== 3 && (i%2) !== 0) {
+                if (mz[i][j] !== "G") {
+                    mz[i][Math.floor(Math.random() * 5)] = "#";
+                }
+            }
         }
     }
+    return mz;
 }
 
+// MOSTRAR LABERINTO
+function mostrar() {
+    for (let i = 0; i < maze.length; i++) {
+        console.log(maze[i]);
+    }
+}
+
+// RESOLVER LABERINTO
 function resol(mz, x, y) {
-    if (x == -1 || y == -1 || x == mz.length+1 || y == mz.length+1) {
-        return false
+    if (x == -1 || y == -1 || x == mz.length || y == mz.length) {
+        return false;
     }
-    if (mz[x][y] == "G") {
-        return true
+    if (mz[y][x] == "G") {
+        return true;
     }
-    if (mz[x][y] == "#") {
-        return false
+    if (mz[y][x] == "#") {
+        return false;
     }
-    mz[x][y] == "x";
-    if (resol(mz, x, y+1) == true) {
-        return true
+    if (mz[y][x] == "X") {
+        return false;
     }
-    if (resol(mz, x+1, y) == true) {
-        return true
+    if (mz[y][x] == "+") {
+        return false;
     }
-    if (resol(mz, x, y-1) == true) {
-        return true
+    if (mz[y][x] !== "S") {
+        mz[y][x] = "+";
     }
-    if (resol(mz, x-1, y) == true) {
-        return true
+    //ESTE
+    if (resol(mz, x + 1, y) == true) {
+        return true;
+    }
+    //NORTE
+    if (resol(mz, x, y - 1) == true) {
+        return true;
+    }
+    //OESTE
+    if (resol(mz, x - 1, y) == true) {
+        return true;
+    }
+    //SUR
+    if (resol(mz, x, y + 1) == true) {
+        return true;
     }
 
+    if (mz[y][x] !== "S") {
+        mz[y][x] = "X";
+    }
+    return false;
 }
-
-/*
-if (x,y outside maze) 
-return false
-if (x,y is goal) 
-return true
-if (x,y not open)
- return false
-mark x,y as part of solution path
-if (FIND-PATH(North of x,y) == true) 
-return true
-if (FIND-PATH(East of x,y) == true) 
-return true
-if (FIND-PATH(South of x,y) == true) 
-return true
-if (FIND-PATH(West of x,y) == true) 
-return true
-
-
-unmark x,y as part of solution path
-return false
-*/
